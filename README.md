5b1c66d6da8c4d5863b922f9affcaf3bdb4b5dcd
James O'Rourke <james@cloudadvantage.com.au>
2024-02-22 15:43:52 +1100

    Init repo

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# tag-repo-next-action
+# Tag Repo Next Action
+
+This GitHub Action retrieves the latest tag in the format `${env_slug}-${date}-N` where `N` is increasing per day. It's written in JavaScript and uses the npm package manager.
+
+## Prerequisites
+
+- Node.js
+- npm
+
+## Usage
+
+This action requires the following inputs:
+
+- `github_token`: The token from secrets GITHUB_TOKEN.
+- `env-slug`: The environment slug to use.
+- `date`: The date in the format YYYY-MM-DD.
+
+It outputs the next tag to use in the format `${env_slug}-${date}-N`.
+
+To use this action in your GitHub workflow, add the following step:
+
+```yaml
+- name: Tag the repository with next tag
+  uses: otivo-labs/tag-repo-next-action@main
+  with:
+    github_token: ${{ secrets.GITHUB_TOKEN }}
+    env-slug: 'your-env-slug'
+    date: '2022-01-01'
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+
+    const date = core.getInput('date');
+    const token = core.getInput('token');
+    const envSlug = core.getInput('env-slug');
+    console.log(`Date: ${date}`);
+    const context = github.context;
+    const octokit = github.getOctokit(token);
+
+    let page = 1;
+    let hasNextPage = true;
+    const allTags = [];
+
+    while (hasNextPage) {
+        const {data: tags, headers} = await octokit.rest.repo.listTags({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            per_page: 100,
+            page: page
+        });
+
+        allTags.push(...tags.map(tag => tag.name));
+
+        // Check if there is a next page
+        hasNextPage = headers.link && headers.link.includes('rel="next"');
+        page++;
+    }
+
+    console.log(`All Tags: ${JSON.stringify(allTags, null, 2)}`);
+
+    // Get the number of deployments done today (via tag search) and increment count by 1
+    const deploymentCount = allTags.filter(tag => tag.startsWith(`${process.env.ENV_SLUG}-${date}`)).length;
+    const nextDeploymentNumber = deploymentCount + 1;
+
+    const nextTag = `${envSlug}-${date}-${nextDeploymentNumber}`;
+
+    console.log(`Tagging with nextTag: ${nextTag}`);
+
+    await octokit.rest.git.createRef({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        ref: `refs/tags/${nextTag}`,
+        sha: context.sha
+    });
+    core.setOutput("nextTag", nextTag);
+} catch (error) {
+    core.setFailed(error.message);
+}
