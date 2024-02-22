f7290871f9d6f059e8149b490cf378a21d454f17
James O'Rourke <james@cloudadvantage.com.au>
2024-02-22 16:05:55 +1100

    Fix

--- dist/index.js
+++ dist/index.js
@@ -30817,55 +30817,53 @@ const core = __nccwpck_require__(5969);
 const github = __nccwpck_require__(2262);
 
 async function run() {
-    try {
 
-        const date = core.getInput('date');
-        const token = core.getInput('token');
-        const envSlug = core.getInput('env-slug');
-        console.log(`Date: ${date}`);
-        const context = github.context;
-        const octokit = github.getOctokit(token);
+    const date = core.getInput('date');
+    const token = core.getInput('token');
+    const envSlug = core.getInput('env-slug');
+    console.log(`Date: ${date}`);
+    const context = github.context;
+    const octokit = github.getOctokit(token);
 
-        let page = 1;
-        let hasNextPage = true;
-        const allTags = [];
+    let page = 1;
+    let hasNextPage = true;
+    const allTags = [];
 
-        while (hasNextPage) {
-            const {data: tags, headers} = await octokit.rest.repo.listTags({
-                owner: context.repo.owner,
-                repo: context.repo.repo,
-                per_page: 100,
-                page: page
-            });
-
-            allTags.push(...tags.map(tag => tag.name));
-
-            // Check if there is a next page
-            hasNextPage = headers.link && headers.link.includes('rel="next"');
-            page++;
-        }
-
-        console.log(`All Tags: ${JSON.stringify(allTags, null, 2)}`);
-
-        // Get the number of deployments done today (via tag search) and increment count by 1
-        const deploymentCount = allTags.filter(tag => tag.startsWith(`${process.env.ENV_SLUG}-${date}`)).length;
-        const nextDeploymentNumber = deploymentCount + 1;
-
-        const nextTag = `${envSlug}-${date}-${nextDeploymentNumber}`;
-
-        console.log(`Tagging with nextTag: ${nextTag}`);
-
-        await octokit.rest.git.createRef({
+    while (hasNextPage) {
+        const {data: tags, headers} = await octokit.rest.repo.listTags({
             owner: context.repo.owner,
             repo: context.repo.repo,
-            ref: `refs/tags/${nextTag}`,
-            sha: context.sha
+            per_page: 100,
+            page: page
         });
-        core.setOutput("nextTag", nextTag);
-    } catch (error) {
-        core.setFailed(error.message);
+
+        allTags.push(...tags.map(tag => tag.name));
+
+        // Check if there is a next page
+        hasNextPage = headers.link && headers.link.includes('rel="next"');
+        page++;
     }
+
+    console.log(`All Tags: ${JSON.stringify(allTags, null, 2)}`);
+
+    // Get the number of deployments done today (via tag search) and increment count by 1
+    const deploymentCount = allTags.filter(tag => tag.startsWith(`${process.env.ENV_SLUG}-${date}`)).length;
+    const nextDeploymentNumber = deploymentCount + 1;
+
+    const nextTag = `${envSlug}-${date}-${nextDeploymentNumber}`;
+
+    console.log(`Tagging with nextTag: ${nextTag}`);
+
+    await octokit.rest.git.createRef({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        ref: `refs/tags/${nextTag}`,
+        sha: context.sha
+    });
+    core.setOutput("nextTag", nextTag);
 }
+
+run().catch(e => core.setFailed(e.message));
 })();
 
 module.exports = __webpack_exports__;
